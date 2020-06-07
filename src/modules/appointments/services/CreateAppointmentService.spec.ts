af3adc20a801733e273a7ee4a0a719a85157f3b2
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-06-07 00:22:17 -0300

    ajuste teste

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -39,11 +39,11 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+    jest.spyOn(Date, 'now').mockImplementation(() => {
       return new Date(2020, 4, 10, 10).getTime();
     });
 
-    const appointmentDate = new Date(2020, 4, 10, 10);
+    const appointmentDate = new Date(2020, 4, 10, 12);
     await createAppointment.execute({
       date: appointmentDate,
       provider_id: 'fernandocchaves',
